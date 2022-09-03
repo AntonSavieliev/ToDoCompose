78d7c8cf5b9b334ae4a0377a21a7fcf0eec2f995
Anton Savieliev <ant.savelev@gmail.com>
2022-09-03 12:00:32 +0300

    Setup Navigation Component

---
+++ app/src/main/java/com/savieliev/anton/to_docompose/navigation/Screens.kt
@@ -0,0 +1,17 @@
+package com.savieliev.anton.to_docompose.navigation
+
+import androidx.navigation.NavHostController
+import com.savieliev.anton.to_docompose.util.Action
+import com.savieliev.anton.to_docompose.util.Constants.LIST_SCREEN
+
+class Screens(navController: NavHostController) {
+    val list: (Action) -> Unit = { action ->
+        navController.navigate("list/${action.name}") {
+            popUpTo(LIST_SCREEN) { inclusive = true }
+        }
+    }
+
+    val task: (Int) -> Unit = { taskId ->
+        navController.navigate("task/$taskId")
+    }
+}
