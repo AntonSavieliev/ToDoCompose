d9de89319f9b2af4085c9b796fbdf4df8755ba34
Anton Savieliev <ant.savelev@gmail.com>
2022-09-03 12:35:02 +0300

    Add Fab to List screen

--- app/src/main/java/com/savieliev/anton/to_docompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/savieliev/anton/to_docompose/navigation/destinations/ListComposable.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.savieliev.anton.to_docompose.ui.screens.list.ListScreen
 import com.savieliev.anton.to_docompose.util.Constants.LIST_ARGUMENT_KEY
 import com.savieliev.anton.to_docompose.util.Constants.LIST_SCREEN
 
@@ -16,6 +17,6 @@ fun NavGraphBuilder.listComposable(
             type = NavType.StringType
         })
     ) {
-
+        ListScreen(navigateToTaskScreen = navigateToTaskScreen)
     }
 }
