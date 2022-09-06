ec08f6c2e7b45fc08fcb07b375b3049702134db3
Anton Savieliev <ant.savelev@gmail.com>
2022-09-06 16:41:57 +0300

    Observe Database

--- app/src/main/java/com/savieliev/anton/to_docompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/savieliev/anton/to_docompose/ui/screens/list/ListScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -21,6 +23,11 @@ fun ListScreen(
     sharedViewModel: SharedViewModel
 ) {
 
+    LaunchedEffect(key1 = true) {
+        sharedViewModel.getAllTasks()
+    }
+
+    val allTasks by sharedViewModel.allTasks.collectAsState()
     val searchAppBarState: SearchAppBarState by sharedViewModel.searchAppBarState
     val searchTextState: String by sharedViewModel.searchTextState
 
@@ -33,7 +40,10 @@ fun ListScreen(
             )
         },
         content = {
-            ListContent()
+            ListContent(
+                tasks = allTasks,
+                navigateToTaskScreen = navigateToTaskScreen
+            )
         },
         floatingActionButton = {
             ListFab(onFabClicked = navigateToTaskScreen)
