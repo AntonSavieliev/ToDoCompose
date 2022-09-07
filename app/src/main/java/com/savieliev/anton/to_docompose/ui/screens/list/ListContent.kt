880c865e18269c70756f1df96c4cbef6dd812ce8
Anton Savieliev <ant.savelev@gmail.com>
2022-09-07 12:03:31 +0300

    Show Empty Content

--- app/src/main/java/com/savieliev/anton/to_docompose/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/savieliev/anton/to_docompose/ui/screens/list/ListContent.kt
@@ -24,6 +24,18 @@ fun ListContent(
     tasks: List<ToDoTask>,
     navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
+    if (tasks.isEmpty()) {
+        EmptyContent()
+    } else {
+        DisplayTasks(tasks, navigateToTaskScreen)
+    }
+}
+
+@Composable
+fun DisplayTasks(
+    tasks: List<ToDoTask>,
+    navigateToTaskScreen: (taskId: Int) -> Unit
+) {
     LazyColumn {
         items(
             items = tasks,
@@ -78,8 +90,7 @@ fun TaskItem(
                 ) {
                     Canvas(
                         modifier = Modifier
-                            .width(PRIORITY_INDICATOR_SIZE)
-                            .height(PRIORITY_INDICATOR_SIZE)
+                            .size(PRIORITY_INDICATOR_SIZE)
                     ) {
                         drawCircle(
                             color = toDoTask.priority.color
