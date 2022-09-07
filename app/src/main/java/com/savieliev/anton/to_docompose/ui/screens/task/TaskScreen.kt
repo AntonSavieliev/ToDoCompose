33e4e24b38da1ce3b1c12ccee39743932d7a6255
Anton Savieliev <ant.savelev@gmail.com>
2022-09-07 15:13:14 +0300

    Add New Task App Bar

---
+++ app/src/main/java/com/savieliev/anton/to_docompose/ui/screens/task/TaskScreen.kt
@@ -0,0 +1,12 @@
+package com.savieliev.anton.to_docompose.ui.screens.task
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskScreen() {
+    Scaffold(
+        topBar = {},
+        content = {}
+    )
+}
