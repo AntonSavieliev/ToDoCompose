15a11e4d217dbd99eef43c61f8efc88a62044332
Anton Savieliev <ant.savelev@gmail.com>
2022-09-09 17:39:38 +0300

    Add Task

--- app/src/main/java/com/savieliev/anton/to_docompose/data/models/ToDoTask.kt
+++ app/src/main/java/com/savieliev/anton/to_docompose/data/models/ToDoTask.kt
@@ -7,7 +7,7 @@ import com.savieliev.anton.to_docompose.util.Constants.DATABASE_TABLE
 @Entity(tableName = DATABASE_TABLE)
 data class ToDoTask(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val id: Int = 0,
     val title: String,
     val description: String,
     val priority: Priority,
