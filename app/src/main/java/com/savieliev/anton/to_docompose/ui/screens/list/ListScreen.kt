62461c4fedaaefb90b831e265e0dd6e8d4062bb8
Anton Savieliev <ant.savelev@gmail.com>
2022-09-13 11:49:06 +0300

    Swipe to Delete - Implementation

--- app/src/main/java/com/savieliev/anton/to_docompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/savieliev/anton/to_docompose/ui/screens/list/ListScreen.kt
@@ -62,6 +62,10 @@ fun ListScreen(
                 highPriorityTasks = highPriorityTasks,
                 sortState = sortState,
                 searchAppBarState = searchAppBarState,
+                onSwipeToDelete = { action, task ->
+                    sharedViewModel.action.value = action
+                    sharedViewModel.updatedTaskFields(selectedTask = task)
+                },
                 navigateToTaskScreen = navigateToTaskScreen
             )
         },
